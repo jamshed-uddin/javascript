e22cd0e99425c42a23b49bd4d093f28af7228e1f
Jami <jamsheduddin03@gmail.com>
2023-02-01 22:53:01 +0600

    ,

--- module18/conditional.js
+++ module18/conditional.js
@@ -8,8 +8,27 @@ var money = 50;
 //     console.log(' i will not eat the chicken');
 // }
 
-if (chicken <= money)
-{
-    console.log('i will eat');
+// if (chicken <= money)
+// {
+//     console.log('i will eat');
+// }
+// else { console.log(' i will eat chicken');}
+
+
+//nested if else
+
+var number = 30;
+
+if (number == 30){
+if(typeof number == 'string'){
+    console.log('this is equal to 30 and string data type')
 }
-else { console.log(' i will eat chicken');}
+else{
+    console.log('this is 30 but not a string data type')
+}
+
+}
+
+else {
+    console.log('number is not equal to 30')
+}
